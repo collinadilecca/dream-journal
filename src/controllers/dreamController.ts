e50bdb33f7b211b47790269bd0934222c74c7414
Nikola Djikic <nikoladjikic22@gmail.com>
2021-03-19 19:48:39 +0100

    Revert "Add search endpoint"
    This reverts commit 20f6e69c83f5b12043cb6188d763e7bb81c03901.

--- src/controllers/dreamController.ts
+++ src/controllers/dreamController.ts
@@ -1,5 +1,5 @@
-import e, { Router } from "express"
-import { Dream, IDream, DreamType } from "../models/dream";
+import { Router } from "express"
+import { Dream, IDream } from "../models/dream";
 
 const router = Router();
 
@@ -10,7 +10,7 @@ router.get("/", async (req, res) => {
         const dreams: IDream[] = await Dream.find({}).lean();
         res.status(200).json(dreams);
     } catch (error) {
-        res.status(400).send();
+        res.status(500).send();
         console.error(error);
     }
 });
@@ -26,7 +26,7 @@ router.get("/:id", async (req, res) => {
         const foundDream: IDream = await Dream.findById(dreamId).lean();
         res.status(200).json(foundDream)
     } catch (error) {
-        res.status(400).send();
+        res.status(500).send();
         console.error(error);
     }
 });
@@ -38,7 +38,7 @@ router.post("/", async (req, res) => {
         const newDream: IDream = await Dream.create(req.body);
         res.status(201).json(newDream)
     } catch (error) {
-        res.status(400).send();
+        res.status(500).send();
         console.error(error);
     }
 });
@@ -52,10 +52,10 @@ router.put("/:id", async (req, res) => {
         const updatedDream: IDream = await Dream.findByIdAndUpdate(dreamId, req.body, { new: true, useFindAndModify: false}).lean();
         res.status(200).json(updatedDream);
     } catch (error) {
-        res.status(400).send();
+        res.status(500).send();
         console.error(error);
     }
-});
+})
 
 
 // Delete a dream
@@ -65,54 +65,10 @@ router.delete("/:id", async (req, res) => {
         const deletedDream: IDream = await Dream.findByIdAndDelete(dreamId).lean();
         res.status(200).json(deletedDream);
     } catch (error) {
-        res.status(400).send();
+        res.status(500).send();
         console.error(error.message);
     }
-});
-
-// Dream search
-
-router.get("/search", async (req, res) => {
-
-    // Fetching the query parameters
-    let title = req.query.title;
-    let type = req.query.type;
-    let startDate = req.query.startDate;
-    let endDate = req.query.endDate;
-
-    // Helper variables
-    let newStartDate;
-    let newEndDate;
-
-    // Pagination and sorting parameters
-    let itemsPerPage = req.query.itemsPerPage;
-    let sortBy = req.query.sortBy;
-    let order = req.query.order;
-
-   
-    if (title===undefined){
-        title="";
-    }
-    if(startDate!==undefined && endDate!==undefined){
-        newStartDate=new Date(startDate.toString());
-        newEndDate=new Date(endDate.toString());
-    }else{
-        return res.status(400).send({ errMessage: "Insert valid date values" })
-    }
-    const foundDream = await Dream.
-       find({
-        $and:[ 
-            {date:{$gte:newStartDate,$lte:newEndDate}} ,
-            {$or:[{title:{$regex:`.*${title.toString().toLowerCase()}.*`}}]},
-            {$or:[ {type:{$eq: type ? <DreamType>type.toString() : undefined}}]}
-        ]
-       }).limit(itemsPerPage===undefined ? 5 : Number(itemsPerPage))
-       .sort({[sortBy!==undefined ?sortBy.toString() : sortBy="title"]:order}).lean();
-       
-
-    return res.status(200).send(foundDream);
-
-});
+})
 
 
 export default router;
